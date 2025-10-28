d20e20d10464e1c6a35a2e45c51886ef6064916c
dieginin <diegobalestra9@gmail.com>
2025-10-27 23:55:32 -0700

    feat: implement Loading and PopLink components; refactor Router and usePath for improved routing logic

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
-import App from "./App.jsx"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <h1>Hello, world!</h1>
   </StrictMode>
 )
