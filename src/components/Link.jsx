a01fddc655e56840b81685af17df572215010325
dieginin <diegobalestra9@gmail.com>
2025-10-27 21:06:04 -0700

    refactor: reorder Link component props for consistency

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,6 +1,6 @@
 import { navigateTo } from "../utils/paths"
 
-export function Link({ target, to, ...props }) {
+export function Link({ to, target, ...props }) {
   const handleClick = (event) => {
     const isMainEvent = event.button === 0
     const isModifiedEvent =
