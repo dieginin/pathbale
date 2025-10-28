48ed4b8707b9ab54e2a1d08689a1306bf1bca8f8
dieginin <diegobalestra9@gmail.com>
2025-10-27 23:53:55 -0700

    refactor: remove unused components and assets to streamline the application

--- src/pages/Search.jsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react"
-
-export default function SearchPage({ params }) {
-  useEffect(() => {
-    document.title = `Buscando: ${params.query}`
-  }, [params])
-
-  return <h1>Has buscado: {params.query}</h1>
-}
